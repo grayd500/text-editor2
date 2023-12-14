ced56194cce71469f2a5a408d0ac3574bdd663b4
David <grayd500@gmail.com>
2023-12-14 17:05:19 -0500

    update with code for initial server functionality

---
+++ server/routes/htmlRoutes.js
@@ -0,0 +1,7 @@
+// server/routes/htmlRoutes.js:
+const path = require('path');
+
+module.exports = (app) =>
+  app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+  );
