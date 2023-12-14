c6c64cf2d28133e91454fc8854e39e2fbe7b8624
David <grayd500@gmail.com>
2023-12-14 17:13:53 -0500

    Update with more server fixes

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -24,7 +24,10 @@ module.exports = () => {
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
       ],
     },
   };
